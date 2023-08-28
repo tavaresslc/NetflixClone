4a04221fcb330a3faafde94f78c04923883387fe
Lucas Soares Tavares <130789583+tavaresslc@users.noreply.github.com>
2023-08-28 00:14:15 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
+
+export default () => {
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegando o Featured
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
+      setFeaturedData(chosenInfo);
+    };
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
+
+    window.addEventListener("scroll", scrollListener);
+
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
+
+  return (
+    <div className="page">
+      <Header black={blackHeader} />
+
+      {featuredData && <FeaturedMovie item={featuredData} />}
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <div>
+            <MovieRow key={key} title={item.title} items={item.items} />
+          </div>
+        ))}
+      </section>
+
+      <footer>
+        Direitos de imagem para Netflix
+        <br />
+        Dados retirados do site Themoviedb.org
+      </footer>
+
+      {movieList.length <= 0 && (
+        <div className="loading">
+          <img
+            src="https://hips.hearstapps.com/popularmechanics/assets/16/48/1480516731-4f155204-7266-486d-88a5-2018ff11f947.gif"
+            alt="Carregando"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
